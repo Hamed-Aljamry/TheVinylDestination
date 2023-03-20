4e503aa6df3a87dac0a26a0c8f1003c1f2f96a57
hamed aljamry <hamed970@hotmail.com>
2023-03-20 14:54:10 +0000

    cloudnianry done

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -2,4 +2,10 @@ class VinylsController < ApplicationController
   def index
     @vinyls = Vinyl.all
   end
+
+  private
+
+  def vinyl_params
+    params.require(:vinyl).permit(:name, :description, :image, :genre, :artist, :price)
+  end
 end
