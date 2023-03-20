bc2fac10ca6509ca0aaacafe170cc5c4eadb8577
hamed aljamry <hamed970@hotmail.com>
2023-03-20 12:49:14 +0000

    added userid to schema+ worked on seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_095541) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_115548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_095541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "music_url"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_vinyls_on_user_id"
   end
 
+  add_foreign_key "vinyls", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning up database....."
+
+Vinyl.destroy_all
+User.destroy_all
+
+puts "Database cleaned 🧼"
+
+users = []
+10.times do |n|
+  users << User.create!(
+    email: "user#{n + 1}@example.com",
+    password: "password"
+  )
+end
+puts "Users created"
+puts "Creating Vinyls....."
+vinyls = []
+10.times do |n|
+  vinyls << Vinyl.create!(
+    name: "Vinyl #{n + 1}",
+    description: "This is Vinyl #{n + 1}",
+    price: rand(10..50),
+    artist: "Artist #{n + 1}",
+    genre: "Genre #{n + 1}",
+    released_at: Date.today - rand(1..10).years,
+    music_url: "https://example.com/music/#{n+1}.mp3",
+    user: users[n]
+  )
+end
+
+puts "seeds created"
