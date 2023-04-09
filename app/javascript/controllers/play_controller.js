685f608f54ae98bd4f1faa27002971843d12b023
hamed aljamry <hamed970@hotmail.com>
2023-04-09 17:36:10 +0400

    rails/ujs

--- app/javascript/controllers/play_controller.js
+++ app/javascript/controllers/play_controller.js
@@ -1,7 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
+import Rails from '@rails/ujs';
 
-// Connects to data-controller="play"
 export default class extends Controller {
+  static targets = ['audio'];
+
   connect() {
+    this.audioPlayer = this.audioTarget;
+  }
+
+  play(event) {
+    event.preventDefault();
+    const vinylId = this.data.get('vinylId');
+    Rails.ajax({
+      url: `/vinyls/${vinylId}/get_music_url`,
+      type: 'GET',
+      success: (data) => {
+        this.audioPlayer.src = data.music_url;
+        this.audioPlayer.play();
+      },
+      error: (data) => {
+        console.log('Error retrieving music URL');
+      }
+    });
   }
 }
