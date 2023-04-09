51a3db1f2de6610a9b06c0b9d1dcf48b1a8f5664
hamed aljamry <hamed970@hotmail.com>
2023-04-09 16:38:11 +0400

    added track_id to vinyls table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_141500) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_09_123652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_141500) do
     t.datetime "updated_at", null: false
     t.string "music_url"
     t.bigint "user_id", null: false
+    t.string "track_id"
     t.index ["user_id"], name: "index_vinyls_on_user_id"
   end
 
